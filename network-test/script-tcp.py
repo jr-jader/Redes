828d035e1eb99fbf659bbb1a4f1ef1070b10fac0
laistie <aeiltsi03@gmail.com>
2023-11-22 23:20:32 -0300

    script nn funfa de computadores diferentes

--- network-test/script-tcp.py
+++ network-test/script-tcp.py
@@ -1,11 +1,11 @@
 import socket, time, locale
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+locale.setlocale(locale.LC_ALL, '')
 
-SELF_IP = '127.0.0.1'
-PEER_IP = '127.0.0.1'
-PORT = 3030
-PORT2 = 3031
+PEER1_IP = '127.0.0.1'
+PEER2_IP = '127.0.0.1'
+PORT1 = 8000
+PORT2 = 8001
 
 STRING = 'teste de rede *2023*' * 100
 
@@ -14,19 +14,19 @@ PACKET_SIZE = 500
 TIMER = 5
 
 class tcp_upload:
-    def __init__(self, dest):
-        self.sock = socket.socket(socket.AF_INET, #Internet
-                            socket.SOCK_STREAM) #TCP
-        self.sock.connect(dest)
-        print('Now connected to Upload on ', dest)
-
     def start_upload(self, data, dest):
-        start_time = time.time()
-        end_time = 0
-        while time.time() - start_time < TIMER:
-            self.upload(data, dest)
-            end_time = time.time()
-        print(f'START TIME: {start_time} | END TIME: {end_time}')
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect(dest)
+            print('Now connected to Upload on ', dest)
+            start_time = time.time()
+            end_time = 0
+            while time.time() - start_time < TIMER:
+                self.upload(data, dest)
+                end_time = time.time()
+            print(f'START TIME: {start_time} | END TIME: {end_time}')
+        except Exception as e:
+            print(f"Error during upload: {e}")
 
     def send_message(self, message, dest):
         self.sock.sendto(message.encode('utf-8'), dest)
@@ -36,7 +36,7 @@ class tcp_upload:
         for chunk in chunks:
             self.send_message(chunk, dest)
         
-class tcp_download:
+class tcp_download: 
     def __init__(self, dest):
         self.sock = socket.socket(socket.AF_INET, #Internet
                             socket.SOCK_STREAM) #TCP
@@ -46,11 +46,14 @@ class tcp_download:
         self.received_packets = 0
     
     def start_download(self, dest):
-        conn, addr = self.sock.accept()  # Accept a connection
-        print('Now connected to Download from ', dest)
-        start_time = time.time()
-        while time.time() - start_time < TIMER:
-            self.download(conn)
+        try:
+            conn, addr = self.sock.accept()
+            print('Now connected to Download from ', dest)
+            start_time = time.time()
+            while time.time() - start_time < TIMER + 1:
+                self.download(conn)
+        except Exception as e:
+            print(f"Error during download: {e}")
 
     def download(self, conn): #receiver
         data = ''
@@ -69,24 +72,23 @@ def main():
         routine = input("ERROR: invalid routine. Please select your routine\n0 - upload | 1 - download\nYour choice: ")
     if routine == '0':
         print('\n===TCP UPLOAD ROUTINE===\n')
-        obj = tcp_upload((SELF_IP, PORT))
-        obj.start_upload(STRING, (SELF_IP, PORT))
+        obj = tcp_upload()
+        obj.start_upload(STRING, (PEER1_IP, PORT1))
         obj.sock.close()
         print('\n===TCP DOWNLOAD ROUTINE AFTER UPLOAD===\n')
-        obj = tcp_download((PEER_IP, PORT2))
-        obj.start_download((PEER_IP, PORT2))
+        obj = tcp_download((PEER2_IP, PORT2))
+        obj.start_download((PEER2_IP, PORT2))
         obj.sock.close()
         print(f"Total of bytes: {locale.format_string('%d', obj.received_bytes, grouping=True)}\nTotal of packets: {locale.format_string('%d', obj.received_packets, grouping=True)}")
     else:
         print('\n===TCP DOWNLOAD ROUTINE===\n')
-        obj = tcp_download((SELF_IP, PORT))
-        obj.start_download((SELF_IP, PORT))
+        obj = tcp_download((PEER1_IP, PORT1))
+        obj.start_download((PEER1_IP, PORT1))
         obj.sock.close()  # Close the connection when done
         print(f"Total of bytes: {locale.format_string('%d', obj.received_bytes, grouping=True)}\nTotal of packets: {locale.format_string('%d', obj.received_packets, grouping=True)}")
-        time.sleep(1)
         print('\n===TCP UPLOAD ROUTINE AFTER DOWNLOAD===\n')
-        obj = tcp_upload((PEER_IP, PORT2))
-        obj.start_upload(STRING, (PEER_IP, PORT2))
+        obj = tcp_upload()
+        obj.start_upload(STRING, (PEER2_IP, PORT2))
         obj.sock.close()
 
 if __name__ == "__main__":
