717a35c9078d7873091c4c3621d0283377b80b61
Jader Junior <juuniorcaardoso@gmail.com>
2023-11-27 13:02:05 -0300

    udp att

--- network-test/script-udp.py
+++ network-test/script-udp.py
@@ -1,7 +1,6 @@
 import socket
 import time
 import locale
-import threading
 
 locale.setlocale(locale.LC_ALL, '')
 
@@ -14,9 +13,9 @@ STRING = 'teste de rede *2023*' * 100
 
 PACKET_SIZE = 500
 
-TIMER = 5
+TIMER = 5  # Adjusted the timer
 
-class udp_upload:
+class UDPUpload:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -30,12 +29,12 @@ class udp_upload:
             while time.time() - start_time < TIMER:
                 self.upload(data, dest)
             end_time = time.time()
-            return start_time, end_time  # Return start_time and end_time
+            return start_time, end_time
 
         except Exception as e:
             print(f"Error during upload: {e}")
         finally:
-            self.sock.close()  # Close the socket after upload is complete
+            self.sock.close()
 
     def send_message(self, message, dest):
         try:
@@ -49,16 +48,16 @@ class udp_upload:
         chunks = [data[i: i + PACKET_SIZE] for i in range(0, len(data), PACKET_SIZE)]
         for chunk in chunks:
             self.send_message(chunk, dest)
-            time.sleep(0.01)  # Add a small delay to avoid packet loss
+            time.sleep(0.001)
 
-class udp_download:
+class UDPDownload:
     def __init__(self, dest):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(dest)
         self.received_bytes = 0
         self.received_packets = 0
-
+        
     def start_download(self):
         try:
             print(f'Now connected to Download on {self.sock.getsockname()}\n')
@@ -66,58 +65,55 @@ class udp_download:
             while time.time() - start_time < TIMER:
                 self.download()
             end_time = time.time()
-            return start_time, end_time  # Return start_time and end_time
+            return start_time, end_time
 
         except Exception as e:
             print(f"Error during download: {e}")
         finally:
-            self.sock.close()  # Close the socket after download is complete
+            self.sock.close()
 
     def download(self):
         try:
             data, _ = self.sock.recvfrom(PACKET_SIZE)
             self.received_packets += 1
             self.received_bytes += len(data)
+
         except Exception as e:
             print(f"Error receiving message: {e}")
 
 def main():
-    routine = input("Please select your routine\n0 - upload | 1 - download\nYour choice: ")
+    routine = input("Por favor, escolha sua rotina\n0 - upload | 1 - download\nSua escolha: ")
     while not routine.isdigit() or routine not in ['0', '1']:
-        routine = input("ERROR: invalid routine. Please select your routine\n0 - upload | 1 - download\nYour choice: ")
+        routine = input("ERRO: rotina inválida. Por favor, escolha sua rotina\n0 - upload | 1 - download\nSua escolha: ")
 
     if routine == '0':
-        print('===UDP UPLOAD ROUTINE===\n')
-        upload_obj = udp_upload()
-        start_time, end_time = upload_obj.start_upload(STRING, (PEER1_IP, PORT1))
-        upload_obj.sock.close()
-        print(f"Total of bytes sent: {locale.format_string('%d', upload_obj.sent_bytes, grouping=True)} \nTotal of packets sent: {locale.format_string('%d', upload_obj.sent_packets, grouping=True)}")
-        print(f'START TIME: {start_time} | END TIME: {end_time}')
+        print('=== ROTINA DE UPLOAD UDP ===\n')
+        upload_obj = UDPUpload()
+        start_time_upload, end_time_upload = upload_obj.start_upload(STRING, (PEER1_IP, PORT1))
+        print(f"Total de bytes enviados: {locale.format_string('%d', upload_obj.sent_bytes, grouping=True)} \nTotal de pacotes enviados: {locale.format_string('%d', upload_obj.sent_packets, grouping=True)}")
+        print(f'START TIME: {start_time_upload} | END TIME: {end_time_upload}')
 
-        print('\n===UDP DOWNLOAD ROUTINE AFTER UPLOAD===\n')
-        download_obj = udp_download((PEER2_IP, PORT2))
-        start_time, end_time = download_obj.start_download()
-        download_obj.sock.close()
-        print(f"Total of bytes received: {locale.format_string('%d', download_obj.received_bytes, grouping=True)} \nTotal of packets received: {locale.format_string('%d', download_obj.received_packets, grouping=True)}")
-        print(f'START TIME: {start_time} | END TIME: {end_time}')
+
+        print('\n=== ROTINA DE DOWNLOAD UDP APÓS O UPLOAD ===\n')
+        download_obj = UDPDownload((PEER2_IP, PORT2))
+        start_time_download, end_time_download = download_obj.start_download()
+        print(f"Total de bytes recebidos: {locale.format_string('%d', download_obj.received_bytes, grouping=True)} \nTotal de pacotes recebidos: {locale.format_string('%d', download_obj.received_packets, grouping=True)}")
+        print(f'START TIME: {start_time_download} | END TIME: {end_time_download}')
 
     else:
-        print('===UDP DOWNLOAD ROUTINE===\n')
-        download_obj = udp_download((PEER1_IP, PORT1))
-        start_time, end_time = download_obj.start_download()
-        download_obj.sock.close()
-        print(f"Total of bytes received: {locale.format_string('%d', download_obj.received_bytes, grouping=True)} \nTotal of packets received: {locale.format_string('%d', download_obj.received_packets, grouping=True)}")
-        print(f'START TIME: {start_time} | END TIME: {end_time}')
-        
+        print('=== ROTINA DE DOWNLOAD UDP ===\n')
+        download_obj = UDPDownload((PEER1_IP, PORT1))
+        start_time_download, end_time_download = download_obj.start_download()
+        print(f"Total de bytes recebidos: {locale.format_string('%d', download_obj.received_bytes, grouping=True)} \nTotal de pacotes recebidos: {locale.format_string('%d', download_obj.received_packets, grouping=True)}")
+        print(f'START TIME: {start_time_download} | END TIME: {end_time_download}')
 
         time.sleep(2)
 
-        print('\n===UDP UPLOAD ROUTINE AFTER DOWNLOAD===\n')
-        upload_obj = udp_upload()
-        start_time, end_time = upload_obj.start_upload(STRING, (PEER2_IP, PORT2))
-        upload_obj.sock.close()
-        print(f"Total of bytes sent: {locale.format_string('%d', upload_obj.sent_bytes, grouping=True)} \nTotal of packets sent: {locale.format_string('%d', upload_obj.sent_packets, grouping=True)}")
-        print(f'START TIME: {start_time} | END TIME: {end_time}')
+        print('\n=== ROTINA DE UPLOAD UDP APÓS O DOWNLOAD ===\n')
+        upload_obj = UDPUpload()
+        start_time_upload, end_time_upload = upload_obj.start_upload(STRING, (PEER2_IP, PORT2))
+        print(f"Total de bytes enviados: {locale.format_string('%d', upload_obj.sent_bytes, grouping=True)} \nTotal de pacotes enviados: {locale.format_string('%d', upload_obj.sent_packets, grouping=True)}")
+        print(f'START TIME: {start_time_upload} | END TIME: {end_time_upload}')
 
 if __name__ == "__main__":
     main()
